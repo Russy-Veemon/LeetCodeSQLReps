3154ea3ec266c906a4c7032af1011787bea01734
Russell Valencia <russyvsc@gmail.com>
2023-02-03 22:23:20 -0800

    using length to return new columns

---
+++ SQLPracticeFeb323.py
@@ -0,0 +1,23 @@
+# You have access to a table of monsters as follows:
+# ** monsters table schema **
+# id
+# name
+# legs
+# arms
+# characteristics
+# Your task is to make a new table where each column should contain the length of the string in the column to its right (last column should contain length of string in the first column). Remember some column values are not currently strings. Column order and titles should remain unchanged:
+# ** output table schema **
+# id
+# name
+# legs
+# arms
+# characteristics
+
+SELECT 
+  length(name) AS id,
+  length(legs::text) AS name,
+  length(arms::text) AS legs,
+  length(characteristics) AS arms,
+  length(id::text) AS characteristics
+FROM
+  monsters
